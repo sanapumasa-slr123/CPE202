c9b0c511560e69859c70d97456ec1ffdea083001
aguilar <acaguilar@student.hau.edu.ph>
2025-07-18 23:49:09 +0800

    auron

--- CPE202/admin.py
+++ CPE202/admin.py
@@ -1,3 +1,20 @@
-from django.contrib import admin
+# CPE202/models.py (Example)
+from django.db import models
 
-# Register your models here.
+class Portfolio(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    # Add other fields as needed for your portfolio items
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+class Project(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    # Add other fields as needed for your projects
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
